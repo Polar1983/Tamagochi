9989f3d77e1ff182b7a352ad6a48a9fceb52698b
Adrián Ramírez <adraez.o3@gmail.com>
2019-10-15 19:20:03 +0200

    cambio1

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
+class tamagochi{
 
+  constructor(sc, felicidad, energia){
+
+    this.sc= sc
+    this.felicidad= felicidad;
+    this.energia =energia;
+  }
+  duerme(energia){}
+  come(sc, energia){}
+  ocio(felicidad,energia){}
+  get getFelicidad(){}
+  get getEnergia(){}
+  get getSc(){} 
+  
+  set setFelicidad(){}
+  set setEnergia(){}
+  set setSc(){} 
+  
+  
+
+
+}
 
 
 
