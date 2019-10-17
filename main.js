42612448b0ba466f355c3b96f9a1a688f5395796
Antonio Efuman <antonipasual85@gmail.com>
2019-10-17 18:21:20 +0200

    juegos final commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-class Tamagochi{
+class Tamagochi {
 
-    constructor(sc, felicidad, energia)/* Inicializamos ya los valores aqui?*/{
+    constructor(sc, felicidad, energia){
   
       this.sc= sc;
       this.felicidad= felicidad;
@@ -9,10 +9,7 @@ class Tamagochi{
 
     tiempo(tamagochi){
         
-        varName(tamagochi)
-        
-        
-        
+    varName(tamagochi) 
         
     }
 
@@ -43,150 +40,106 @@ class Tamagochi{
 
     
     ocio(felicidad,energia){
-        let opc = '';
-    do {
-    console.log('1. winOrLoose');
-    console.log('2. numbers');
-    console.log('3. riddles');
-    switch(opc){
+        let opc = prompt('1. winOrLoose'+ '\n'+'2. numbers'+ '\n'+'3. riddles');
+        switch (opc) {
         case '1' :
-            console.log('Jugar a winOrLoose');
-            winOrLoose();
+            this.winOrLoose();
             break;
         case '2' :
-            console.log('Jugar a numbers');
-            numbers();
+            this.numbers();
             break;
         case '3' :
-            console.log('Jugar a riddles');
-            riddles();
+            this.riddles();
             break;
-            
+        }
+
+    }  // Fin metodo ocio
+
+    winOrLoose (energia,felicidad) {
+        let rand = prompt('Adivina el numero escondido entre el 1 y 10')
+        for(let i = 0; i < 1; i++){
+            if(i === rand){
+                this.felicidad += 30;
+                this.energia += 1;
+                alert('win');
+            } else {
+                 this.felicidad += -50;
+                 this.energia += -1;
+                 alert('loose');
+            }
+        }
     }
-    opc = prompt('Introduce un numero entre 1 y 3 : ') ;
-    } while(opc === '1' || opc === '2' || opc === '3'); 
-    
-}
+
+
+     numbers (energia,felicidad) {
+        let input1 = Math.floor(Math.random() * 10);
+        let input2 = Math.floor(Math.random() * 10);
+        let sum = input1 + input2;
+        let answer = parseInt(prompt(`Cuanto es ${input1} + ${input2} `));
+         if (answer === sum){
+            this.felicidad += 30;
+            this.energia += 1;
+             alert('win');
+         } else {
+            this.felicidad -= 30;
+            this.energia -= 1;
+             alert('loose');
+        }
+      
+    }
+
+    riddles (adiv,answ) {  
+        let riddle = [{guest :'Mi tía Cuca tiene una mala racha, ¿quién será esta muchacha?', answer: 'cucaracha'},{guest :'Canto en la orilla, vivo en el agua, no soy pescado, ni soy cigarra.', answer : 'rana'},{guest :'¿Qué animal tiene las cinco vocales?', answer : 'murcielago'},{guest :'Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera.', answer: 'pera' }];
+        let randomRiddle = Math.floor(Math.random() * riddle.length);
+        for(let i = 0; i < riddle.length; i++){
+            let rid = alert(riddle[i].guest);
+            let ans = prompt('Introduce tu respuesta : ');
+            if(riddle[i].answer === ans){
+                document.write(`La solucion a la adivinanza ${riddle[i].guest} Es : ${riddle[i].answer}`);
+                document.write('You win 1 riddle');
+                this.felicidad += 50;
+                this.energia += 1;
+                
+            } else {
+                document.write('Try again another riddle answer');
+                this.felicidad += -50;
+                this.energia += -1;
+            }
+        }
+    }    
 
 
     
+
+    
 }
-
-
 
 var varName = function(tamagochi){
     var varCounter = 0;
     let hambre = 100;
     setInterval(function(){
-        document.body.innerHTML = ''
+        document.body.innerHTML = '';
        if( varCounter>24)
       {console.log('finish');varCounter=0;}
        if (tamagochi.getSc < 40)
-         {
-      
-       
+        {
        tamagochi.come();
-
-        
            }
+        else if (tamagochi.getFelicidad === 70){
+            tamagochi.ocio();
+        }
       document.write("<br/>Son las " ,varCounter)
       document.write("<br/>Nivel de hambre: ", tamagochi.getSc)
       document.write("<br/>Nivel de energia: ", tamagochi.getEnergia)
       document.write("<br/>Nivel de felicidad: ", tamagochi.getFelicidad)
       
       
-      ;varCounter++;tamagochi.restahambre = tamagochi.getSc; tamagochi.restaenergia = tamagochi.getEnergia; tamagochi.restafelicidad = tamagochi.getFelicidad}, 1000);
+      ;varCounter++;tamagochi.restahambre = tamagochi.getSc; tamagochi.restaenergia = tamagochi.getEnergia; tamagochi.restafelicidad = tamagochi.getFelicidad
+    }, 1000);}
 
 
-}
 
+let tamagochi = new Tamagochi(100,100,100);
+tamagochi.tiempo(tamagochi);
 
- 
-function numbers (num1,num2,operator) {
-    let input1 = parseInt(prompt('Introduce un numero : '));
-    let input2 = parseInt(prompt('Introduce un numero : '));
-    let randomOp = prompt('Introduce un operador : +,-,*,/');
-    var op = ['+','-','/','*'];
-    for(let i = 0; i < op.length; i++){
-        if(op[0] === randomOp){
-            let operator = '+';
-        }else if (op[1] === op[randomOp]){
-           let operator = '-';
-        }else if (op[2] === op[randomOp]) {
-           let operator = '/';
-        }else if (op[3] === op[randomOp]){
-           let operator = '*';
-        }
-    }
-     switch(randomOp){
-         case '+':
-             var sum = input1 + input2;
-             var answersum = console.log(`Estas sumando ${input1} + ${input2}, y  es igual a ${sum}`);
-             time.setFelicidad = 50;
-             time.setEnergia = 1;
-             console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-             return answersum;
-             break;
-         case '-':
-             let rest = input1 - input2;
-             let answerrest = console.log(`Estas restando ${input1} - ${input2}, y  es igual a ${rest}`);;
-             time.setFelicidad = 50;
-             time.setEnergia = 1;
-             console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-             return answerrest;
-             break;
-         case '/':
-                let div = input1 / input2;
-                let answerdiv = console.log(`La division no tiene resultado}`);
-                if(input1 % input2 === 0){
-                time.setFelicidad = -50;
-                time.setEnergia = -1;
-                console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-                return answerdiv;
-                break;
-             } else if (input1 === 0 || input2 === 0){
-                time.setFelicidad = -50;
-                time.setEnergia = -1;
-                console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-                 return answer;
-             }
-               
-         case '*' :
-             let mult = input1 * input2;
-             let answer = console.log(`Estas multiplicando ${input1} * ${input2}, y  es igual a ${mult}`);
-             time.setFelicidad = 50;
-                time.setEnergia = 1;
-                console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-             return answer;
-             break;
-         default :
-            return console.log('No has introducido un operador correcto : +,-,/,*,');
-  
-        }
-        
- }
-
- function riddles (adiv,answ) {
-    let riddle = [{guest :'Mi tía Cuca tiene una mala racha, ¿quién será esta muchacha?', answer: 'cucaracha'},{guest :'Canto en la orilla, vivo en el agua, no soy pescado, ni soy cigarra.', answer : 'rana'},{guest :'¿Qué animal tiene las cinco vocales?', answer : 'murcielago'},{guest :'Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera.', answer: 'pera' }];
-    let randomRiddle = Math.floor(Math.random() * riddle.length);
-    for(let i = 0; i < riddle.length; i++){
-        let rid = console.log(riddle[i].guest);
-        let ans = prompt('Introduce tu respuesta : ');
-        if(riddle[i].answer === ans){
-            console.log(`La solucion a la adivinanza ${riddle[i].guest} Es : ${riddle[i].answer}`);
-            console.log('You win 1 riddle');
-            time.setFelicidad = 50;
-            time.setEnergia = 1;
-            console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-            
-        } else {
-            console.log('Try again another riddle answer');
-            time.setFelicidad = -50;
-            time.setEnergia = -1;
-            console.log(`Tu Felicidad es de : ${time.getFelicidad} y tu Energia es de : ${time.energia}`);
-            
-        }
-    }
-}
-
-
+    
